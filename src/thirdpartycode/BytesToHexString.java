8fd28e6b34a1dcf6fbfd0a4fbe0ea4162865d056
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-03-13 17:08:38 +0100

    Replacing Java's implementation of bytes to hexadecimal string.
    This change is due to Java dropping support of the module previously
    used from Java 9 and newer versions.

---
+++ src/thirdpartycode/BytesToHexString.java
@@ -0,0 +1,33 @@
+package thirdpartycode;
+
+/*
+ * Copyright notice
+ * 
+ * This code was take from Stack Overflow. I do not own any right on it and this
+ * class is published under the same license as found in stack overflow, which
+ * is the Creative Commons Attribution Share Alike license.
+ * 
+ * Original post: https://stackoverflow.com/a/9855338
+ * 
+ * Author name: maybeWeCouldStealAVan
+ * Author profile: https://stackoverflow.com/users/1284661/maybewecouldstealavan
+ */
+
+public class BytesToHexString {
+	private BytesToHexString() {
+		// Private constructor
+	}
+	
+	public static String getHexString(byte[] bytes) {
+		final char[] hexArray = "0123456789ABCDEF".toCharArray();
+		char[] hexChars = new char[bytes.length * 2];
+		
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		
+		return new String(hexChars);
+	}
+}
