ba8731dd9b50d8f8d62bb68c6ad3d91185b95fdf
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-04-11 21:48:19 +0200

    Editado el import del BytesToHexString.

--- src/standalone/SantaloneApp.java
+++ src/standalone/SantaloneApp.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import thirdpartycode.BytesToHexString;
+import converter.BytesToHexString;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.swing.GroupLayout;
