ca2c7a14061aa091abd46a0e34378fa71e733c00
Radu Dumitru Boboia <rd.boboia@alumnos.upm.es>
2019-04-11 21:54:07 +0200

    Implementada una plantilla para los idiomas.

---
+++ src/languages/MessagesTemplate.java
@@ -0,0 +1,51 @@
+package languages;
+
+/**
+ * English messages for the {@link Messages} class.
+ * <br>
+ * Made as a template to copy, paste and edit for easier implementation of a
+ * new translation.
+ * 
+ * @author -$BOSS$-
+ */
+public class MessagesTemplate implements Messages {
+	public String getMatches(int matches) {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+
+	public String getCongratulations() {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+
+	public String getNoMatchesFound() {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+
+	public String getBadNews() {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+
+	public String getTimesPasswordFound(int times) {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+
+	public String getComputingHash() {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+
+	public String getMakingRequestAndProcessingResponse() {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+
+	public String getMalformedUrlExceptionMessage() {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+
+	public String getSha1HashingAlgorithmNotFound() {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+
+	public String getUnexpectedIOException() {
+		return Messages.MESSAGE_NOT_FOUND;
+	}
+}
